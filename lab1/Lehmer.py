2d188eec7adf3ed2a34e6f1b74739f9c54a897ef
prague <prague15031939@gmail.com>
2021-09-05 00:18:00 +0300

    refactorings, added period calculation

--- lab1/Lehmer.py
+++ lab1/Lehmer.py
@@ -1,6 +1,3 @@
-from typing import Sequence
-
-
 class Lehmer:
 
     def __init__(self, multiplier, start, modulus):
@@ -8,17 +5,29 @@ class Lehmer:
         self.R = start
         self.m = modulus
 
+        self.x = None
+        self.__precision = 0.0001
+        self.periodValue = None
+
     def GetSequence(self):
+        self.x = self.R
+
         sequence = [self.GetNext()]
         temp = self.GetNext()
-        while all(abs(item - temp) > 0.0001 for item in sequence):
+        while all(abs(item - temp) > self.__precision for item in sequence):
             sequence.append(temp)
             temp = self.GetNext() 
+        else:
+            self.periodValue = temp
         return sequence
 
+    def GetSequenceOf(self, count):
+        self.x = self.R
+        return [self.GetNext() for i in range(count)]
+
     def GetNext(self):
-        self.R = self.a * self.R % self.m
-        return  self.R / self.m
+        self.x = self.a * self.x % self.m
+        return self.x / self.m
 
     def GetFrequencies(self, sequence, intervalAmount):
         sequence.sort()
@@ -37,4 +46,10 @@ class Lehmer:
             frequencies.append(hitNumber / sequenceLength)
         
         return frequencies
+
+    def GetPeriod(self):
+        testSequence = self.GetSequenceOf(10 ** 5)
+        v = testSequence[-1]
+        hits = [i for i, item in enumerate(testSequence) if abs(item - v) < self.__precision]
+        return hits[1] - hits[0], hits[1]
 
--- lab1/lab1.py
+++ lab1/lab1.py
@@ -3,40 +3,44 @@ from Lehmer import Lehmer
 import matplotlib.pyplot as plt
 import numpy as np
 from math import pi
-from Lehmer import Lehmer
-
-# R0 = 44293
-# a = 2117183648
-# m = 4292917296
 
 intervalAmount = 20
 
 def main():
-    startValue = 44293#int(input("R0: "))
-    multiplier = 2117183648#int(input("a: "))
-    modulus = 4292917296#int(input("m: "))
+    variant = input("\n0 - max uniformity\n1 - period > 50000\nother - own values\n> ")
+    if int(variant) == 0:
+        startValue = 44293
+        multiplier = 84589
+        modulus = 217728
+    elif int(variant) == 1:
+        startValue = 20823
+        multiplier = 4095
+        modulus = 2147483647
+    else:
+        startValue = int(input("R0: "))
+        multiplier = int(input("a: "))
+        modulus = int(input("m: "))
 
     generator = Lehmer(multiplier, startValue, modulus)
     sequence = generator.GetSequence()
     unsortedSequence = sequence.copy()
     frequencies = generator.GetFrequencies(sequence, 20)
 
-    plt.bar([x + 1 / intervalAmount for x in np.arange(0, 1, 1 / intervalAmount)], frequencies, width = 1 / intervalAmount)
+    plt.bar([x + 1 / intervalAmount for x in np.arange(0, 1, 1 / intervalAmount)], frequencies, width = 1 / intervalAmount, edgecolor="black")
     plt.show()
 
     statistics = Statistics()
     expectation = statistics.GetExpectation(sequence)
-    print(f"math expectation: {expectation}")
-
     dispersion = statistics.GetDispersion(sequence, expectation)
-    print(f"dispersion: {dispersion}")
-
-    print(f"mean square deviation: {np.sqrt(dispersion)}")
 
     manualCheckValue, check = statistics.ManualCheck(unsortedSequence)
+    period, aperiodicLenght = generator.GetPeriod()
 
-    print(f"manual check is {manualCheckValue}, seeks to Pi/4 ({pi / 4}): {check}")
-    
+    print(f"\nmath expectation: {expectation}")
+    print(f"dispersion: {dispersion}")
+    print(f"mean square deviation: {np.sqrt(dispersion)}")
+    print(f"manual check: {manualCheckValue}, seeks to Pi/4 ({pi / 4}): {check}")
+    print(f"sequence period: {period}, aperiodic length: {aperiodicLenght}\n")
 
 if __name__ == "__main__":
     main()
